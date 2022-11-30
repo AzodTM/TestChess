865e572c806c04cc5d71fc55474224d05355d834
Федотов Андрей Сергеевич <azodtm@gmail.com>
2022-11-30 21:03:48 +0400

    Refactored

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@
     {
         int[] startPosition = new int[2];
         int[] endPosition = new int[2];
+        int[] path;
         var rnd = new Random();
         while (true)
         {
@@ -11,35 +12,52 @@
             startPosition[1] = rnd.Next(7);
             endPosition[0] = rnd.Next(7);
             endPosition[1] = rnd.Next(7);
+
+            PrintChessBoard(startPosition, endPosition);
+            path = GetPath(startPosition, endPosition);
+
             Console.WriteLine("Start position is [{0},{1}] \nEnd position is [{2},{3}]",
                 startPosition[0], startPosition[1], endPosition[0], endPosition[1]);
-            PrintChessBoard(startPosition, endPosition);
-            Console.WriteLine("Step by Bishop is {0}", IsPossibleForBishopMove(startPosition, endPosition));
-            Console.WriteLine("Step by Knight is {0}", IsPossibleForKnightMove(startPosition, endPosition));
+            
+            Console.WriteLine("Step by Bishop is {0}", IsPossibleForBishopMove(path));
+            Console.WriteLine("Step by Knight is {0}", IsPossibleForKnightMove(path));
             Console.WriteLine("Press any key to continue");            
             Console.WriteLine("You pressed {0}",Console.ReadKey());
             Console.Clear();
         }
     }
-    static private bool IsPossibleForBishopMove(int[] startPosition, int[] endPosition)
+    static private bool IsPossibleForBishopMove(int[] path)
     {
-        var moveX = endPosition[0] - startPosition[0];
-        var moveY = endPosition[1] - startPosition[1];
-        if (moveX != 0 && Math.Abs(moveX) == Math.Abs(moveY))
+        return IsPossibleDiagonalMove(path);
+    }
+    static private bool IsPossibleForKnightMove(int[] path)
+    {        
+        if (path[0] != path[1] && (path[0] == 1 && path[1] == 2) || (path[0] == 2 && path[1] == 1))
+            return true;
+        return false;
+    }
+    static private bool IsPossibleDiagonalMove(int[] path)
+    {        
+        if (path[0] != 0 && path[0] == path[1])
         {
             return true;
         }
         return false;
     }
-    static private bool IsPossibleForKnightMove(int[] startPosition, int[] endPosition)
+    static private bool IsPossibleStraightMovie(int[] path)
+    {
+        return path[0] == path[1];
+    }
+    static private int[] GetPath(int[] startPosition, int[] endPosition)
     {
         var moveX = Math.Abs(endPosition[0] - startPosition[0]);
         var moveY = Math.Abs(endPosition[1] - startPosition[1]);
-        if (moveX != moveY && (moveX== 1 && moveY == 2) || (moveX == 2 && moveY == 1))
-            return true;
-        return false;
+        return new int[] { moveX, moveY };
     }
-   
+    static private bool MoveMoreThanAero(int[] path)
+    {
+       return path[0] + path[1] > 0;
+    }
     static private void PrintChessBoard(int[] startPosition, int[] endPosition)
     {
         var sizeOfChessBoard = 8;
