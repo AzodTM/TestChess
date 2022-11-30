9ade6acd23c77e21582a693ac6a46a659abef9c2
Федотов Андрей Сергеевич <azodtm@gmail.com>
2022-11-30 22:41:57 +0400

    Added all figures on assignment

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,13 +14,19 @@
             endPosition[1] = rnd.Next(7);
 
             PrintChessBoard(startPosition, endPosition);
+            Console.WriteLine();
+
             path = GetPath(startPosition, endPosition);
 
             Console.WriteLine("Start position is [{0},{1}] \nEnd position is [{2},{3}]",
                 startPosition[0], startPosition[1], endPosition[0], endPosition[1]);
+            Console.WriteLine();
             
             Console.WriteLine("Step by Bishop is {0}", IsPossibleForBishopMove(path));
             Console.WriteLine("Step by Knight is {0}", IsPossibleForKnightMove(path));
+            Console.WriteLine("Step by Rock is {0}", IsPossibleForRockMove(path));
+            Console.WriteLine("Step by King is {0}", IsPossibleForKingMove(path));
+            Console.WriteLine("Step by Queen is {0}", IsPossibleForQueenMove(path));
             Console.WriteLine("Press any key to continue");            
             Console.WriteLine("You pressed {0}",Console.ReadKey());
             Console.Clear();
@@ -28,25 +34,33 @@
     }
     static private bool IsPossibleForBishopMove(int[] path)
     {
-        return IsPossibleDiagonalMove(path);
+        return IsPossibleDiagonalMove(path) && MoveMoreThanZero(path);
     }
     static private bool IsPossibleForKnightMove(int[] path)
-    {        
-        if (path[0] != path[1] && (path[0] == 1 && path[1] == 2) || (path[0] == 2 && path[1] == 1))
-            return true;
-        return false;
+    {
+        return MoveMoreThanZero(path) && path[0] != path[1] && (path[0] == 1 && path[1] == 2) || (path[0] == 2 && path[1] == 1);
+         
+    }
+    static private bool IsPossibleForRockMove(int[] path)
+    {
+        return IsPossibleStraightMovie(path);
+    }
+    static private bool IsPossibleForKingMove(int[] path)
+    {
+        return (path[0] <2 && path[1] < 2) && MoveMoreThanZero(path);
+    }
+    static private bool IsPossibleForQueenMove(int[] path)
+    {
+        return IsPossibleStraightMovie(path) || IsPossibleDiagonalMove(path);
     }
     static private bool IsPossibleDiagonalMove(int[] path)
-    {        
-        if (path[0] != 0 && path[0] == path[1])
-        {
-            return true;
-        }
-        return false;
+    {
+        return  MoveMoreThanZero(path) && path[0] == path[1];
+        
     }
     static private bool IsPossibleStraightMovie(int[] path)
     {
-        return path[0] == path[1];
+        return (path[0] == 0 || path[1] == 0) && MoveMoreThanZero(path);
     }
     static private int[] GetPath(int[] startPosition, int[] endPosition)
     {
@@ -54,7 +68,7 @@
         var moveY = Math.Abs(endPosition[1] - startPosition[1]);
         return new int[] { moveX, moveY };
     }
-    static private bool MoveMoreThanAero(int[] path)
+    static private bool MoveMoreThanZero(int[] path)
     {
        return path[0] + path[1] > 0;
     }
@@ -67,13 +81,13 @@
             {
                 if (i == startPosition[1] && j == startPosition[0])
                 {
-                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.ForegroundColor= ConsoleColor.Green;
                     Console.Write("* ");
                     Console.ForegroundColor = ConsoleColor.White;
                 }
                 else if(i == endPosition[1] && j == endPosition[0])
                 {
-                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.Write("* ");
                     Console.ForegroundColor = ConsoleColor.White;
                 }
